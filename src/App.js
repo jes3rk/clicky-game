3f0acff1f27d0b70701cee9494d3e3bc6e6acd96
Joseph Schefer <joseph.schefer@gmail.com>
2018-01-08 13:01:55 -0500

    adding things to make it work

--- src/App.js
+++ src/App.js
@@ -5,32 +5,56 @@ import ImagePanel from "./components/Image";
 
 class App extends Component {
   state = {
+    options: [
+      {
+        src: "http://media.guitarcenter.com/is/image/MMGS7/2017-Les-Paul-Standard-T-Electric-Guitar-Honey-Burst/J45014000002000-00-500x500.jpg/",
+        alt: "Les Paul Standard Honey Burst"
+      },
+      {
+        src: "http://media.musiciansfriend.com/is/image/MMGS7/FA-135CE-Cutaway-Concert-Acoustic-Electric-Guitar-Natural/H70301000001000-00-500x500.jpg",
+        alt: "Cutaway Concert Acoustic Electric"
+      },
+      {
+        src: "https://www.dawsons.co.uk/media/catalog/product/cache/1/image/1200x/6b9ffbf72458f4fd2d3cb995d92e8889/y/a/yamaha_f310_acoustic_guitar_-_natural_-_main.jpg",
+        alt: "yamaha f310"
+      }
+    ],
     idClicked: [],
     score: 0
-  }
+  };
 
   click = id => {
+    let arr = this.state.idClicked;
     if (this.state.idClicked.length === 0) {
       let newScore = this.state.score + 1;
+      arr.push(id);
       this.setState({
-        score: newScore
+        score: newScore,
+        idClicked: arr
       });
     } else {
       let j = 0;
+      console.log(id);
+      console.log(this.state.idClicked);
       for (var i = 0; i < this.state.idClicked.length; i++) {
         if (this.state.idClicked[i] === id) {
           j++;
         };
-        if (i === this.state.idClicked.length - 1 && j === 0) {
-          let newScore = this.state.score + 1;
-          this.setState({
-            score: newScore,
-            idClicked: this.state.idClicked.push(id)
-          });
+        if (i === this.state.idClicked.length - 1) {
+          if (j === 0) {
+            let newScore = this.state.score + 1;
+            arr.push(id);
+            this.setState({
+              score: newScore,
+              idClicked: arr
+            });
+          } else {
+            console.log("loss")
+          };
         };
       };
     };
-  }
+  };
 
   render() {
     return (
@@ -42,7 +66,8 @@ class App extends Component {
             <p>Score: {this.state.score}</p>
           </div>
         </div>
-        <ImagePanel src="http://media.guitarcenter.com/is/image/MMGS7/2017-Les-Paul-Standard-T-Electric-Guitar-Honey-Burst/J45014000002000-00-500x500.jpg/" id="1" alt="Les Paul Standard Guitar" click={this.click}/>
+        <div className="row">
+        </div>
       </div>
     );
   }
--- src/components/Image.js
+++ src/components/Image.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ImagePanel = props => (
   <div>
-    <img src={props.src} className="clicky-image" dataId={props.id} onClick={props.click(props.id)} alt={props.alt}/>
+    <img src={props.src} className="clicky-image" dataid={props.id} onClick={(e) => props.click(e.target.getAttribute('dataid'))} alt={props.alt}/>
   </div>
 );
 
